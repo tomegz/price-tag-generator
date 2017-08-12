ba1ea01a3fb232634910de534315fe118c4088da
tomegz <tomaszbubala@o2.pl>
2017-08-12 09:20:38 +0200

    add route from login to app

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const LoginForm = () => {
-  return (
-    <form>
-      <input type="text" placeholder="profi-bike" />
-      <input type="password" />
-      <input type="submit" value="Login" />
-    </form>
-  );
+class LoginForm extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const store = this.storeInput.value;
+    this.context.router.history.push(`/${store}`);
+  }
+  render() {
+    return (
+      <form className="login-form" onSubmit={this.handleSubmit}>
+        <input type="text" defaultValue="profi-bike" ref={(input) => { this.storeInput = input }} />
+        <input type="password" />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
+
+LoginForm.contextTypes = {
+  router: PropTypes.object
 }
 
 export default LoginForm;

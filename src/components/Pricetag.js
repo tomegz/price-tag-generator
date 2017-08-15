8e799e48860ea89443b1d7ee8f75bcce1f1c2620
tomegz <tomaszbubala@o2.pl>
2017-08-15 22:13:56 +0200

    add discount price logic

--- src/components/Pricetag.js
+++ src/components/Pricetag.js
@@ -4,20 +4,24 @@ import "../styles/Pricetag.css";
 class Pricetag extends React.Component {
   render() {
     const { details } = this.props;
+    const onDiscount = details.discountStatus === "on";
+    const discountPrice = onDiscount ? <p>{details.discountPrice},-</p> : "";
     return (
       <div>
         <div className="half-tag">
           <div className="text-content">
             <h2>{details.name}</h2>
             <h4>{details.model}</h4>
-            <p>{details.price},-</p>
+            <p className={onDiscount ? "before-discount" : ""}>{details.price},-</p>
+            {discountPrice}
           </div>
         </div>
         <div className="half-tag">
           <div className="text-content">
             <h2>{details.name}</h2>
             <h4>{details.model}</h4>
-            <p>{details.price},-</p>
+            <p className={onDiscount ? "before-discount" : ""}>{details.price},-</p>
+            {discountPrice}
           </div>
         </div>
       </div>

81423bf195edfd910a941714f01dedb4334d535d
tomegz <tomaszbubala@o2.pl>
2017-09-08 21:08:56 +0200

    adding auto scroll to order div

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
 import "../styles/Order.css";
-import CSSTransitionGroup from "react-addons-css-transition-group";
+import { CSSTransitionGroup } from "react-transition-group";
 
-const Order = ({items, order, removeFromOrder}) => {
-  const renderOrder = (key) => {
+class Order extends Component {
+  constructor() {
+    super();
+    this.renderOrder = this.renderOrder.bind(this);
+    this.scrollToBottom = this.scrollToBottom.bind(this);
+  }
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+  scrollToBottom() {
+    const list = this._orderList;
+    list.scrollTop = list.scrollHeight - list.clientHeight;
+  }
+  renderOrder(key) {
+    const { items, order, removeFromOrder } = this.props;
     const item = items[key];
     const count = order[key];
     const removeButton = <button onClick={() => removeFromOrder(key)}>&times;</button>;
@@ -25,19 +41,21 @@ const Order = ({items, order, removeFromOrder}) => {
             </CSSTransitionGroup>
             szt.
           </strong> 
-          {item.name} {item.model}
+        {item.name} {item.model}
         </span>
         {removeButton}
       </li>
     );
-  };
-  const orderIds = Object.keys(order);
+  }
+  render() {
+    const { order } = this.props;
+    const orderIds = Object.keys(order);
     const total = orderIds.reduce((prevTotal, key) => {
       const count = order[key] || 0;
       return prevTotal + count;
     }, 0);
-  return (
-      <div className="order">
+    return (
+      <div className="order" ref={(c) => this._orderList = c}>
         <h2>Ceny do druku</h2>
         <button className="print-button" onClick={() => window.print()}><i className="fa fa-print fa-3x" /></button>
         <CSSTransitionGroup className="order-list" 
@@ -45,14 +63,15 @@ const Order = ({items, order, removeFromOrder}) => {
                             transitionName="order"
                             transitionEnterTimeout={500}
                             transitionLeaveTimeout={500}>
-          {orderIds.map(renderOrder)}
+          {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Ilość cen do druku: </strong>{total}
           </li>
         </CSSTransitionGroup>
       </div>
-  );
-};
+    );
+  }
+}
 
 Order.propTypes = {
   items: PropTypes.object.isRequired,

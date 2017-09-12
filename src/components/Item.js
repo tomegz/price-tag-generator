c519ea0783d0d057d76d7b309054bac9d7abf752
tomegz <tomaszbubala@o2.pl>
2017-09-12 22:18:44 +0200

    improve menu css

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,11 +4,21 @@ import PropTypes from "prop-types";
 const Item = ({details, index, addToOrder}) => {
   const isOnDiscount = details.discountStatus === "on";
   return (
-    <li className="menu-item">
-        <p><strong>{`${details.name} ${details.model}`}</strong></p>
-        <span className="price">{details.price}</span>
-        {isOnDiscount ? <span className="discountPrice">{details.discountPrice}</span> : ""}
-        <button onClick={() => addToOrder(index)}>+</button>
+      <li className="menu-item">
+        <div>
+          <div className="desc">  
+            <h5><strong>{`${details.name} ${details.model}`}</strong></h5>
+            <p><i className="fa fa-calendar" /> 2017</p>
+            <p>
+              <i className="fa fa-money" />
+              <span className="price"> {details.price} </span>
+              {isOnDiscount ? <span className="discount-price">{details.discountPrice}</span> : ""}
+            </p>
+          </div>
+          <div className="adding-section">
+            <button onClick={() => addToOrder(index)}>+</button>
+          </div>
+        </div>
       </li>
   );
 };

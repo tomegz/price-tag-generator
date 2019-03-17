93182aa0806db00f61615fcb6871e35adf696222
Tomasz Bubała <tomasz.bubala@brainhub.pl>
2019-03-17 15:37:45 +0100

    Generate available years dynamically, refactor AddItemForm

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -1,42 +1,97 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const AddItemForm = ({addItem}) => {
-  const createItem = (e) => {
-    e.preventDefault();
-    const item = {
-      name: this.name.value,
-      model: this.model.value,
-      price: Number(this.price.value),
-      year: this.year.value,
-      discountPrice: Number(this.discountPrice.value),
-      discountStatus: this.discountStatus.value
-    };
-    addItem(item);
-    this.itemForm.reset();
-  };
-  return (
-    <div className="add-item-wrapper">
-        <h4>Dodaj nową cenę</h4>
-        <form ref={(input) => this.itemForm = input} className="add-item" onSubmit={createItem}>
-          <input ref={(input) => this.name = input} type="text" placeholder="Marka" required />
-          <input ref={(input) => this.model = input} type="text" placeholder="Model" required />
-          <select ref={(input) => this.year = input}>
-            <option value="2018">2018</option>
-            <option value="2017">2017</option>
-            <option value="2016">2016</option>
-          </select>
-          <input ref={(input) => this.price = input} type="text" placeholder="Cena" required />
-          <select ref={(input) => this.discountStatus = input}>
-            <option value="on">Promocja włączona</option>
-            <option value="off">Promocja wyłączona</option>
-          </select>
-          <input ref={(input) => this.discountPrice = input} type="text" placeholder="Cena promocyjna" required />
-          <button type="submit"><strong>+ DODAJ</strong></button>
-        </form>
-      </div>
-  );
+const getCurrentYear = () => {
+  return new Date().getFullYear();
 };
+
+const CURRENT_YEAR = getCurrentYear();
+
+const getAvailableYears = () => {
+  return [
+    CURRENT_YEAR - 3,
+    CURRENT_YEAR - 2,
+    CURRENT_YEAR -1,
+    CURRENT_YEAR,
+    CURRENT_YEAR + 1,
+  ];
+};
+
+const AVAILABLE_YEARS = getAvailableYears();
+const AVAILABLE_OPTIONS = ['-', ...AVAILABLE_YEARS];
+
+const initialState = {
+  name: '',
+  price: '',
+  model: '',
+  discountPrice: '',
+  year: CURRENT_YEAR,
+  discountStatus: '',
+}
+
+class AddItemForm extends React.Component {
+  state = {
+    ...initialState,
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState(state => ({
+      ...state,
+      [name]: value,
+    }));
+  };
+
+  createItem = (e) => {
+    const { addItem } = this.props;
+
+    addItem({
+      name: this.state.name,
+      model: this.state.model,
+      price: Number(this.state.price),
+      year: this.state.year,
+      discountPrice: Number(this.state.discountPrice),
+      discountStatus: this.state.discountStatus,
+    });
+    this.resetForm();
+    e.preventDefault();
+  };
+
+  resetForm = () => {
+    this.setState({ ...initialState });
+  }
+
+  render() {
+    const {
+      name,
+      model,
+      price,
+      year,
+      discountPrice,
+      discountStatus,
+    } = this.state;
+
+    return (
+      <div className="add-item-wrapper">
+          <h4>Dodaj nową cenę</h4>
+          <form className="add-item" onSubmit={this.createItem}>
+            <input name="name" value={name} onChange={this.handleChange} type="text" placeholder="Marka" required />
+            <input name="model" value={model} onChange={this.handleChange} type="text" placeholder="Model" required />
+            <select name="year" value={year} onChange={this.handleChange}>
+              {AVAILABLE_OPTIONS.map(year => <option key={year} value={year}>{year}</option>)}
+            </select>
+            <input name="price" value={price} onChange={this.handleChange} type="text" placeholder="Cena" required />
+            <select name="discountStatus" value={discountStatus} onChange={this.handleChange}>
+              <option value="on">Promocja włączona</option>
+              <option value="off">Promocja wyłączona</option>
+            </select>
+            <input name="discountPrice" value={discountPrice} onChange={this.handleChange} type="text" placeholder="Cena promocyjna" required />
+            <button type="submit"><strong>+ DODAJ</strong></button>
+          </form>
+        </div>
+    );
+  }
+}
 
 AddItemForm.propTypes = {
   addItem: PropTypes.func.isRequired
